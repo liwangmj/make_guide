b623dc184e9691f74d0266f391dfeeeeed4d22a8
Wim <liwangmj@gmail.com>
2015-11-11 22:22:36 +0800

    修改windows动态库的问题

--- cmake/include/example_dll/example_dll.h
+++ cmake/include/example_dll/example_dll.h
@@ -8,11 +8,21 @@
 #ifndef _INCLUDE_EXAMPLE_DLL_EXAMPLE_DLL_H
 #define _INCLUDE_EXAMPLE_DLL_EXAMPLE_DLL_H
 
+#if defined(__WIN32)
+	#if defined(__DLL_LIBRARY)
+	#  define DLL_SHARED_EXPORT __declspec(dllexport)
+	#else
+	#  define DLL_SHARED_EXPORT __declspec(dllimport)
+	#endif
+#else
+	#define DLL_SHARED_EXPORT
+#endif
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-int example_dll_test();
+DLL_SHARED_EXPORT int example_dll_test();
 
 #ifdef __cplusplus
 }
