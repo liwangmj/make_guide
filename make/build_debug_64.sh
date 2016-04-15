d52b89c1b5561b972ada4171258a4920682ec429
Wim <liwangmj@gmail.com>
2016-04-15 11:29:14 +0800

    删除

--- make/build_debug_64.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-cd ${PWD}
-make CVER=debug clean
-make CVER=release clean
-make CVER=debug
-
-exit 0
--- make/build_release_64.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-cd ${PWD}
-make CVER=debug clean
-make CVER=release clean
-make CVER=release
-
-exit 0
