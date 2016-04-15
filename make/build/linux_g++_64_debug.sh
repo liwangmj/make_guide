0cac179c7461cf4d0ba21e6398377535ab5c6230
Wim <liwangmj@gmail.com>
2016-04-15 10:21:20 +0800

    添加其他目录

---
+++ make/build/linux_g++_64_debug.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+cd ${PWD}/../
+make CVER=debug CCPU=64 CPLATFORM=linux CCOMPILER=g++ clean
+make CVER=release CCPU=64 CPLATFORM=linux CCOMPILER=g++ clean
+make CVER=debug CCPU=64 CPLATFORM=linux CCOMPILER=g++
+
+exit 0
