2fd73e3d02a89e10639129b866a97da97a19e275
Wim <liwangmj@gmail.com>
2016-04-15 11:24:14 +0800

    修复windows下文件格式，让linux可运行

--- make/build/linux_g++_64_debug.sh
+++ make/build/linux_g++_64_debug.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 cd ${PWD}/../
+
 make CVER=debug CCPU=64 CPLATFORM=linux CCOMPILER=g++ clean
 make CVER=release CCPU=64 CPLATFORM=linux CCOMPILER=g++ clean
 make CVER=debug CCPU=64 CPLATFORM=linux CCOMPILER=g++
