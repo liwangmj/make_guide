a52f663dd43ed07a66d9f5647b21619610765ae2
Wim <liwangmj@gmail.com>
2016-04-24 22:08:42 +0800

    修复__WIN32宏为WIN32 修改windows下的编译脚本

--- cmake/build/win32_msvc_32_release.bat
+++ cmake/build/win32_msvc_32_release.bat
@@ -4,8 +4,8 @@ rd /s /q %~dp0..\..\build\cmake_win32_msvc_32_release
 md %~dp0..\..\build\cmake_win32_msvc_32_release\
 cd %~dp0..\..\build\cmake_win32_msvc_32_release\
 
-cmake -DCVER=release -DCCPU=32 -DCCOMPILE=msvc -DCPLATFORM=win32 -G"Visual Studio 10" ..\..\cmake
-devenv cmake.sln /rebuild "Release|Win32"
+cmake -DCVER=release -DCCPU=32 -DCCOMPILE=msvc -DCPLATFORM=win32 -G"Visual Studio 10" -DCMAKE_CONFIGURATION_TYPES=release ..\..\cmake
+devenv ALL_BUILD.vcxproj /rebuild "release|Win32"
 ::xcopy E:\SourceCode\bin\linux_g++_64\debug E:\SourceCode\eclipse\app_bin\dnsserver /s /e /y
 
 cd ..
--- cmake/include/example_dll/example_dll.h
+++ cmake/include/example_dll/example_dll.h
@@ -8,7 +8,7 @@
 #ifndef _INCLUDE_EXAMPLE_DLL_EXAMPLE_DLL_H
 #define _INCLUDE_EXAMPLE_DLL_EXAMPLE_DLL_H
 
-#if defined(__WIN32)
+#if defined(WIN32)
 	#if defined(__DLL_LIBRARY)
 	#  define DLL_SHARED_EXPORT __declspec(dllexport)
 	#else
