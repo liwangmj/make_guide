5adb442b68fd03dc9d2defe352e2359f36d62ee2
Wim <liwangmj@gmail.com>
2016-04-28 21:38:22 +0800

    修改成变量模式

--- cmake/build/ios_x86-64_clang_release.sh
+++ cmake/build/ios_x86-64_clang_release.sh
@@ -4,7 +4,7 @@ mkdir -p ${PWD}/../../build/cmake_ios_x86-64_clang_release/
 cd ${PWD}/../../build/cmake_ios_x86-64_clang_release/
 rm -rf *
 
-cmake -DCPLATFORM=ios -DCCPU=x86-64 -DCCOMPILE=clang -DCVER=debug -G"Xcode" -DCMAKE_BUILD_TYPE=Release -DCMAKE_CONFIGURATION_TYPES=release -DCMAKE_TOOLCHAIN_FILE=../../cmake/toolchain/iOS.cmake -DIOS_PLATFORM=SIMULATOR64 ../../cmake 
+cmake -DCPLATFORM=ios -DCCPU=x86-64 -DCCOMPILE=clang -DCVER=release -G"Xcode" -DCMAKE_BUILD_TYPE=Release -DCMAKE_CONFIGURATION_TYPES=release -DCMAKE_TOOLCHAIN_FILE=../../cmake/toolchain/iOS.cmake -DIOS_PLATFORM=SIMULATOR64 ../../cmake 
 xcodebuild -configuration "Release"
 #cmake --build .
 
